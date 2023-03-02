ff7cd9025e8e637322d0e5e3f704e17f533dadc3
kilodecite <kilodecite@gmail.com>
2023-03-03 00:27:46 +0200

    First

---
+++ main.py
@@ -0,0 +1,40 @@
+items = []
+
+while True:
+    
+    while True:
+        item = input("Input you item: ")
+        item_convert = item.replace(".", "", 1)
+
+        if item_convert.isdigit():
+            item = float(item)
+            break
+        else:
+            print("Please inputt correct digital")
+
+    add = input("stop(s)/next(n)/exit(e): ")
+    items.append(item)
+
+    if add == "s":
+        total = 0
+        for price in items:
+            total = total + price
+        print(total)
+        items.clear()
+
+        while True:
+            question = input("Countine program? (y/n): ")
+
+            if question == "y":
+                break
+            elif question == "n":
+                quit()
+            else:
+                print("Command not found!")
+
+    elif add == "n":
+        ...   
+    elif add == "e":
+        break
+    else:
+        print("Command not found!")
