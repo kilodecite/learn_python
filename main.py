7ff19945bc5bb886b5881640a6c70010e333cb46
kilodecite <kilodecite@gmail.com>
2023-03-03 12:03:02 +0200

    name & index

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+accounts = []
 items = []
-
 loops = [5, 2, 5, 2, 2]
 
 for x in loops:
@@ -9,11 +9,41 @@ for x in loops:
     print(string)
 
 while True:
-    
+    user = input("Input name: ")
+    accounts.append(user)
+    command = input("stop(s)/next(n)/exit(e): ")
+    if command == "s":
+        print(accounts)
+        break
+    elif command == "n":
+        ...
+    elif command == "e":
+        quit()
+    else:
+        print("Command not found!")
+
+while True:
+    index = input("Input index: ")
+    if int(index) > len(accounts):
+        print("ellement incorrect!")
+    elif index.isdigit():
+        print(accounts[int(index)])
+        answer = input("stop(s)/next(n)/exit(e): ")
+        if answer == "s":
+            break
+        elif answer == "n":
+            ...
+        elif answer == "e":
+            quit()
+        else:
+            print("Please inputt correct command!")
+    else:
+        print("Please inputt correct index")
+
+while True:
     while True:
         item = input("Input you item: ")
         item_convert = item.replace(".", "", 1)
-
         if item_convert.isdigit():
             item = float(item)
             break
@@ -29,10 +59,8 @@ while True:
             total = total + price
         print(total)
         items.clear()
-
         while True:
             question = input("Countine program? (y/n): ")
-
             if question == "y":
                 break
             elif question == "n":
